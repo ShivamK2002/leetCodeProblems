89b419cb7470b3eead7fdfd356baab385b68a587
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2023-10-05 10:31:19 +0530

    Time: 9 ms (61.98%) | Memory: 16.4 MB (9.38%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+         int count1 = 0;
+         int count2 = 0;
+        int ele1;  
+        int ele2;  
         int n = nums.size();
-        int count = 1;
-        vector<int> finalAns;
-
-        unordered_map<int,int>mpp;
         for(auto i:nums){
-            mpp[i]++;
+            if(count1==0 && i!=ele2) {
+                count1=1;
+                ele1 = i;
+            }
+            else if(count2==0 && i!=ele1) {
+                count2=1;
+                ele2 = i;
+            }
+            else if(ele1==i) count1++;
+            else if(ele2==i) count2++;
+            else{
+                count1--;
+                count2--;
+            }
+            
         }
-        for(auto i:mpp){
-            if(i.second>n/3) finalAns.push_back(i.first);
-        }
-     
-        return finalAns;
+
+        count1 = 0 ;
+        count2 = 0 ;
+         for(auto i:nums){
+             if(i==ele1) count1++;
+             else if(i==ele2) count2++;
+
+         }        
+        if(count1>n/3) ans.push_back(ele1);
+        if(count2>n/3) ans.push_back(ele2);
+
+        return ans;
+
+
     }
 };
