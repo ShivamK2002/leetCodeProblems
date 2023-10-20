3db5b0dc047f9cc13f545e048f2e523d11f90015
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2023-10-20 12:33:26 +0530

    Time: 31 ms (55.59%) | Memory: 19.4 MB (41.38%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-    // static bool cmp(pair<int,int>a,pair<int,int>b){
-    //     return a.first<b.first;
-    // }
     vector<vector<int>> merge(vector<vector<int>>& arr) {
         vector<vector<int>>ans;
         sort(arr.begin(),arr.end());
-       int n = arr.size();
-    //    for(int i=0; i<n; i++){
-    //        int start = arr[i][0];
-    //        int end = arr[i][1];
-    //        if(!ans.empty() && ans.back()[1]>=arr[i][1]) continue;
-    //        for(int j=i+1; j<n; j++){
-    //            if(end>=arr[j][0]) end = max(end,arr[j][1]);
-    //        }
-
-    //        ans.push_back({start,end});
-            
-
-    //    }
+        int n = arr.size();
         for(int i=0; i<n; i++){
             if(ans.empty() || ans.back()[1]<arr[i][0]) ans.push_back({arr[i][0],arr[i][1]});
             else{
-                ans.back()[1] = max(arr[i][1],ans.back()[1]);
+                ans.back()[0] = min(ans.back()[0],arr[i][0]);
+                ans.back()[1] = max(ans.back()[1],arr[i][1]);
             }
-
-
         }
         return ans;
     }
