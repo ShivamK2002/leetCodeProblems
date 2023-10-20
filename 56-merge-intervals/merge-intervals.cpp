d66b23aa7247ef0ccdf34150eabb838fc9c733f0
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2023-10-20 12:25:20 +0530

    Time: 30 ms (62.81%) | Memory: 19.3 MB (64.65%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        stack<pair<int,int>>st;
-        int n = intervals.size();
-        sort(intervals.begin(),intervals.end());
-        st.push({intervals[0][0],intervals[0][1]});
-        
-        for(int i=1; i<n; i++){
-            if(intervals[i][0]<=st.top().second){
-                int ele1 = st.top().first;
-                int ele2 = intervals[i][1];
-                if(st.top().second > ele2) ele2 = st.top().second;
-                st.pop();
-                st.push({ele1,ele2});
-            }
-            else st.push({intervals[i][0],intervals[i][1]});
-        }
-
+    // static bool cmp(pair<int,int>a,pair<int,int>b){
+    //     return a.first<b.first;
+    // }
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
         vector<vector<int>>ans;
-        while(!st.empty()){
-            int ele1 = st.top().first;
-            int ele2 = st.top().second;
-            ans.push_back({ele1,ele2});
-            st.pop();
+        sort(arr.begin(),arr.end());
+       int n = arr.size();
+    //    for(int i=0; i<n; i++){
+    //        int start = arr[i][0];
+    //        int end = arr[i][1];
+    //        if(!ans.empty() && ans.back()[1]>=arr[i][1]) continue;
+    //        for(int j=i+1; j<n; j++){
+    //            if(end>=arr[j][0]) end = max(end,arr[j][1]);
+    //        }
+
+    //        ans.push_back({start,end});
+            
+
+    //    }
+        for(int i=0; i<n; i++){
+            if(ans.empty() || ans.back()[1]<arr[i][0]) ans.push_back({arr[i][0],arr[i][1]});
+            else{
+                ans.back()[1] = max(arr[i][1],ans.back()[1]);
+            }
+
+
         }
         return ans;
     }
