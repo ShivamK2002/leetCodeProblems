4b597eccdf6644560a8f236210f8026bd166d836
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2023-10-27 12:03:34 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (16.05%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,19 +1,52 @@
 class Solution {
 public:
-    int rec(int n,vector<int>&fib){
-     if(n==0 || n==1){
-         fib[n] = n;
-         return n;
-     }
-     else if(fib[n]!=-1 ) return fib[n];
-     fib[n] = rec(n-1,fib) + rec(n-2,fib);
-     return fib[n];
+ int bottomUp(int n)
+    {
+        // 1. Create dp array
+        vector<int> dp(n+1,-1);
+
+        // 2. Base Case
+        dp[0] = 0;
+        if(n == 0) return dp[0];
+        dp[1] = 1;
+        if(n == 1) return dp[1];
+
+        // 3.
+        for(int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+    int tab(int n,vector<int>&dp){
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
 
     }
-    int fib(int n) {
-        int ans = 0;
+    // int mem(int n,vector<int>&dp){
+    //  if(n==0 || n==1){
+    //      dp[n] = n;
+    //      return n;
+    //  }
+    //  else if(dp[n]!=-1 ) return dp[n];
+    //  dp[n] = mem(n-1,dp) + mem(n-2,dp);
+    //  return dp[n];
 
-        vector<int>fib(n+1,-1);
-        return rec(n,fib);
+    // }
+    int fib(int n) {
+        // int ans = 0;
+
+        // vector<int>dp(n+1,-1);
+        // dp[0] = 0;
+        // dp[1] = 1;
+    
+        //  for(int i=2; i<=n; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+        return bottomUp(n);
     }
 };
