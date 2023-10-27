d21e83a4f0dfbfa980c6204dc30907bdf744153d
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2023-10-27 11:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (16.05%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
+    int rec(int n,vector<int>&fib){
+     if(n==0 || n==1){
+         fib[n] = n;
+         return n;
+     }
+     else if(fib[n]!=-1 ) return fib[n];
+     fib[n] = rec(n-1,fib) + rec(n-2,fib);
+     return fib[n];
+
+    }
     int fib(int n) {
-        if(n==0 || n==1) return n;
-        return fib(n-1)+fib(n-2);
+        int ans = 0;
+
+        vector<int>fib(n+1,-1);
+        return rec(n,fib);
     }
 };
