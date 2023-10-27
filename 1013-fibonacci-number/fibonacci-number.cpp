5b86cb76ce8b0582c0274fa31854a98b0a51535d
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2023-10-27 12:12:26 +0530

    Time: 2 ms (77.62%) | Memory: 6.5 MB (6.49%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -21,15 +21,25 @@ public:
     int fib(int n) {
         // int ans = 0;
 
-        vector<int>dp(n+1,-1);
-        dp[0] = 0;
-        if(n==0) return 0;
-        dp[1] = 1;
-        if(n==0) return 1;
+        // vector<int>dp(n+1,-1);
+        // dp[0] = 0;
+        // if(n==0) return 0;
+        // dp[1] = 1;
+        // if(n==0) return 1;
     
-         for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        //  for(int i=2; i<=n; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+
+        int prev2 = 0;
+        int prev1 = 1;
+        if(n==prev1 || n==prev2) return n;
+        for(int i=2; i<n; i++){
+            int ele = prev1+prev2;
+            prev2 = prev1;
+            prev1 = ele;
         }
-        return dp[n];
+        return prev1+prev2;
     }
 };
