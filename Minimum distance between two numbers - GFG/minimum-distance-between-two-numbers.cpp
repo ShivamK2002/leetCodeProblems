36ff593ef87c728eb8dac3b5a1300af223055d4b
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2023-11-02 11:35:37 +0530

    Added solution - LeetHub

---
+++ Minimum distance between two numbers - GFG/minimum-distance-between-two-numbers.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+  public:
+    int minDist(int arr[], int n, int x, int y) {
+        // code here
+        int ans = INT_MAX;
+        int a = -1;
+        int b= -1;
+        
+        for(int i=0; i<n; i++){
+            if(arr[i]==x) a = i;
+            else if(arr[i]==y) b = i;
+            if(a!=-1 and b!=-1) ans = min(ans,abs(a-b));
+        }
+        if(ans==INT_MAX) return -1;
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        int x, y;
+        cin >> x >> y;
+        Solution obj;
+        cout << obj.minDist(a, n, x, y) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
