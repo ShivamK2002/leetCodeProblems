a64671d24015123c4c28d1e3b8cbfff74a65ffaf
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2023-11-03 14:13:18 +0530

    Time: 7 ms (9.03%) | Memory: 8.7 MB (12.70%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,int n,vector<vector<int>>&ans,unordered_map<int,bool>&mpp,int i,vector<int>temp){
+    void solve(vector<int>&nums,int n,vector<vector<int>>&ans,unordered_map<int,bool>&mpp,vector<int>temp){
         if(temp.size()==n){
             ans.push_back(temp);
             return;
@@ -9,7 +9,7 @@ public:
             if(!mpp[nums[i]]){
                 mpp[nums[i]] = true;
                 temp.push_back(nums[i]);
-                solve(nums,n,ans,mpp,i+1,temp);
+                solve(nums,n,ans,mpp,temp);
                 temp.pop_back();
                 mpp[nums[i]] = false;
             }
@@ -20,7 +20,7 @@ public:
         int n = nums.size();
         vector<int>temp;
         unordered_map<int,bool>mpp;
-        solve(nums,n,ans,mpp,0,temp);
+        solve(nums,n,ans,mpp,temp);
         return ans;
     }
 };
