28dfcf0f89a35960606bc966652dcc2c13d8b15f
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2024-02-06 08:57:35 +0530

    Time: 33 ms (46.51%) | Memory: 24.3 MB (28.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,18 +3,9 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>>mpp;
         for(auto i:strs){
-            
             string temp = i;
             sort(temp.begin(),temp.end());
-
              mpp[temp].push_back(i);   
-            // if(mpp.find(temp)!=mpp.end()){
-            // }
-            // else {
-            //     vector<string> store;
-            //     store.push_back(i);
-            //     mpp[temp] = store;
-            // }
         }      
 
         vector<vector<string>>ans;
