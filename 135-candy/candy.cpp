c7a2d4c4faddb16ece0e2b69a920f023d72c9cdb
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2024-02-23 16:35:42 +0530

    Time: 17 ms (47.12%) | Memory: 21.5 MB (5.01%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,35 +2,38 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int>left(n+1,0);
-        vector<int>right(n+1,0);
+      
 
+        vector<int> left;
+        vector<int> right(n, 0);
 
-        left[0] = 1;
+        left.push_back(0);
         int candies = 1;
-        for(int i=1; i<n; i++){
-            if(ratings[i]>ratings[i-1]){
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
                 candies++;
+            } else {
+                candies = 1;
             }
-            else candies = 1;
-            left[i] = (candies);
+            left.push_back(candies);
         }
-        right[n-1] = 1;
-         candies = 1;
-        for(int i=n-2; i>=0; i--){
-            if(ratings[i]>ratings[i+1]){
+
+        right[n - 1] = 1;
+        candies = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
                 candies++;
+            } else {
+                candies = 1;
             }
-            else candies = 1;
             right[i] = candies;
         }
+
         candies = 0;
-        for(int i=0; i<n; i++){
-            candies+=max(left[i],right[i]);
+        for (int i = 0; i < n; i++) {
+            candies += max(left[i], right[i]);
         }
 
-
         return candies;
-
     }
 };
