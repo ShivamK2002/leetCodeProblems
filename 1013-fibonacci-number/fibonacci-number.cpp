524776a00dfc833fe017c600c43b14d1e4a61039
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2024-02-29 18:01:38 +0530

    Time: 19 ms (5.40%) | Memory: 7.2 MB (8.39%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,45 +1,17 @@
 class Solution {
 public:
+    int memo(int n,vector<int>&dp){
+        if(n<2) return n;
+        if(dp[n]!=-1) return dp[n];
 
-    // int tab(int n,vector<int>&dp){
-    //     for(int i=2; i<=n; i++){
-    //         dp[i] = dp[i-1] + dp[i-2];
-    //     }
-    //     return dp[n];
+        return memo(n-1,dp)+memo(n-2,dp);
+    }
 
-    // }
-    // int mem(int n,vector<int>&dp){
-    //  if(n==0 || n==1){
-    //      dp[n] = n;
-    //      return n;
-    //  }
-    //  else if(dp[n]!=-1 ) return dp[n];
-    //  dp[n] = mem(n-1,dp) + mem(n-2,dp);
-    //  return dp[n];
 
-    // }
     int fib(int n) {
-        // int ans = 0;
-
-        // vector<int>dp(n+1,-1);
-        // dp[0] = 0;
-        // if(n==0) return 0;
-        // dp[1] = 1;
-        // if(n==0) return 1;
-    
-        //  for(int i=2; i<=n; i++){
-        //     dp[i] = dp[i-1] + dp[i-2];
-        // }
-        // return dp[n];
-
-        int prev2 = 0;
-        int prev1 = 1;
-        if(n==prev1 || n==prev2) return n;
-        for(int i=2; i<n; i++){
-            int ele = prev1+prev2;
-            prev2 = prev1;
-            prev1 = ele;
-        }
-        return prev1+prev2;
+        vector<int>dp(n+1,-1);
+        // fib(n-1)+fib(n-2);
+        return memo(n,dp);
+        
     }
 };
