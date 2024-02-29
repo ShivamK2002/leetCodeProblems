ba76826ce3375a5d36fd6b396ecc6405fd13ae6c
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2024-02-29 18:07:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (8.39%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,13 +4,15 @@ public:
         if(n<2) return n;
         if(dp[n]!=-1) return dp[n];
 
-        return memo(n-1,dp)+memo(n-2,dp);
+       dp[n-1] = memo(n-1,dp); 
+       dp[n-2] = memo(n-2,dp);
+       dp[n] = dp[n-1] + dp[n-2];
+       return dp[n];
     }
 
 
     int fib(int n) {
         vector<int>dp(n+1,-1);
-        // fib(n-1)+fib(n-2);
         return memo(n,dp);
         
     }
