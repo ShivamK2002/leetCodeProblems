d5d57b6b05ce79c5823ef262643e35f5e2528de3
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2024-02-29 18:33:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (12.94%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int prev2 = 0;
-        int prev1 = 1;
+    int tabulation(int n, vector<int>&dp){
+        dp [1] = 1;
+        dp [0] = 1;
+        if(n<2) return dp[n] = 1;
 
-        for(int i = 0; i<n; i++){
-            int ele = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = ele;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
+        return dp[n];
 
-        return prev1;
+    }
+    // int memo(int n, vector<int>&dp){
+    //     if(n<2) return dp[n] = 1;
+    //     return dp[n] = memo(n-1,dp) + memo(n-2,dp);
+
+    // }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return tabulation(n,dp);
+        // return climbStairs(n-1) + climbStairs(n-2);
     }
 };
