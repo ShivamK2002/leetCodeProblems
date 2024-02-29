895e8849534d737b04084ac216a4f1fe45b90d2b
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2024-02-29 18:19:53 +0530

    Time: 2 ms (78.10%) | Memory: 7.1 MB (47.07%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
-    int memo(int n,vector<int>&dp){
-        if(n<2) return n;
-        if(dp[n]!=-1) return dp[n];
 
-       dp[n-1] = memo(n-1,dp); 
-       dp[n-2] = memo(n-2,dp);
-       dp[n] = dp[n-1] + dp[n-2];
-       return dp[n];
-    }
+   
+    // int tabulation(int n,vector<int>&dp){
+    //     dp[0] = 0;
+    //     if(n==0) return dp[n];
+    //     dp[1] = 1;
+
+    //     for(int i=2; i<=n; i++){
+    //         dp[i] = dp[i-1] + dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
+    // int memo(int n,vector<int>&dp){
+    //     if(n<2) return n;
+    //     if(dp[n]!=-1) return dp[n];
+
+    //    dp[n-1] = memo(n-1,dp); 
+    //    dp[n-2] = memo(n-2,dp);
+    //    dp[n] = dp[n-1] + dp[n-2];
+    //    return dp[n];
+    // }
 
 
     int fib(int n) {
-        vector<int>dp(n+1,-1);
-        return memo(n,dp);
+        // vector<int>dp(n+1,-1);
+        // return tabulation(n,dp);
+
+        if(n<2) return n;
+        int a = 0;
+        int b = 1;
+        for(int i=2; i<=n; i++){
+            int temp = a;
+            a = b;
+            b = temp+b;
+        }
+        return b;
         
     }
 };
