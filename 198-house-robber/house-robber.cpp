fc818113da0e8926593bc5052673954219177e93
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2024-03-04 17:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (8.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,28 +1,15 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int solve(vector<int>& nums, int index, int n, vector<int>& dp) {
-        if (index >= n) return 0;
 
-        // Check if the result for the current state is already memoized
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-
-        int ans1 = solve(nums, index + 1, n, dp);
-        int ans2 = nums[index] + solve(nums, index + 2, n, dp);
-
-        // Memoize the result for the current state
-        dp[index] = max(ans1, ans2);
-
-        return dp[index];
+    int solve(vector<int>nums,int index,int n,vector<int>&dp){
+        if(index>=n)  return 0;
+        if(dp[index]!=-1) return dp[index];
+        return dp[index] = max(nums[index]+solve(nums,index+2,n,dp), solve(nums,index+1,n,dp));
     }
 
+
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1); // Initialize the dp vector with -1 for memoization
-        return solve(nums, 0, n, dp);
+        vector<int>dp(nums.size()+1,-1);
+        return solve(nums,0,nums.size(),dp);
     }
 };
