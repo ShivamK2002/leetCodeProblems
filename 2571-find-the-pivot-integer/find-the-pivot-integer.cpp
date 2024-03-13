6c7d45c942125aaf7544d6087af294d791284062
Shivam Kawale <91909928+ShivamK2002@users.noreply.github.com>
2024-03-13 17:56:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (73.82%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int tot=n*(n+1)/2;
-        int sum=0;
+        
+        int sum = 0;
+        int totalSum = n*(n+1)/2;
         for(int i=1; i<=n; i++){
-           
-            if(sum==tot-sum-i)return i;
-            sum+=i;    
+           if(sum==totalSum-sum-i) return i;
+            sum+=i;
         }
         return -1;
     }
